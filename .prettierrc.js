21131312c9bf2044b6394bd6254edde48d0018aa
hiriski <hiriski@outlook.com>
2023-11-26 23:16:00 +0700

    install packages & update eslint config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,13 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
   singleQuote: true,
+  jsxSingleQuote: true,
   trailingComma: 'all',
+  arrowParens: 'always',
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  printWidth: 120,
+  endOfLine: 'auto',
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,34 @@
+const path = require('path');
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.json',
+        ],
+        alias: {
+          '@': path.resolve(__dirname, 'src'),
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'ENV',
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
+  ],
 };
